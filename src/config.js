dc1ee3723d3859f6737834868ce22c4d74943fbf
Lucas Desouza <lucasfds2777@gmail.com>
2021-10-08 10:56:04 -0500

    break out apikey retrival

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export function getAPIKey() {
+    return "letmein";
+}
--- src/services/invitation.service.js
+++ src/services/invitation.service.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
+import {getAPIKey} from "../config";
 
 export async function getAllInvitations() {
-    const apiKey = "letmein";
+    const apiKey = getAPIKey()
     const res = await axios.get(`http://localhost:8081/api/v1/invitations?api_key=${apiKey}`);
     console.log(res);
     return res.data;
 }
 
 export async function createInvitation(name, guestCount) {
-    const apiKey = "letmein";
+    const apiKey = getAPIKey()
     await axios.post(
         `http://localhost:8081/api/v1/invitations?api_key=${apiKey}`,
         {
