a604b9458f146fe9d3b8aaef8dca97960c7b5d0d
Lucas Desouza <lucasfds2777@gmail.com>
2021-10-12 23:48:21 -0500

    improve responsiveness in form

--- src/components/NewInvitationForm.js
+++ src/components/NewInvitationForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, TextField, Button} from "@mui/material";
+import {Container, TextField, Button, Grid} from "@mui/material";
 import {Form, Formik} from "formik";
 import {createInvitation} from "../services/invitation.service";
 
@@ -20,28 +20,36 @@ function NewInvitationForm({onSubmit}) {
             >
                 {formik => (
                     <Form>
-                        <TextField
-                            {...formik.getFieldProps('name')}
-                            label="Invitation Name"
-                            variant="outlined"
-                            style={{margin: "1rem"}}
-                        />
-                        <TextField
-                            type="number"
-                            {...formik.getFieldProps('guestCount')}
-                            label="Guest Count"
-                            variant="outlined"
-                            style={{margin: "1rem"}}
-                        />
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            color="primary"
-                            size="large"
-                            style={{margin: "1.5rem"}}
-                        >
-                            Add
-                        </Button>
+                        <Grid container spacing={2}>
+                            <Grid item md={6} xs={12}>
+                                <TextField
+                                    {...formik.getFieldProps('name')}
+                                    label="Invitation Name"
+                                    variant="outlined"
+                                    style={{width: "90%"}}
+                                />
+                            </Grid>
+                            <Grid item md={2} xs={12}>
+                                <TextField
+                                    type="number"
+                                    {...formik.getFieldProps('guestCount')}
+                                    label="Guest Count"
+                                    variant="outlined"
+                                    style={{width: "90%"}}
+                                />
+                            </Grid>
+                            <Grid item md={4} xs={12}>
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    color="primary"
+                                    size="large"
+                                    style={{margin: ".5rem 0", width: "90%"}}
+                                >
+                                    Add
+                                </Button>
+                            </Grid>
+                        </Grid>
                     </Form>
                 )}
             </Formik>
