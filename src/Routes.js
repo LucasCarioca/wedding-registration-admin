59dda851bad3f4959a332b363e94243a9dd5d604
Lucas Desouza <lucasfds2777@gmail.com>
2021-10-14 22:19:25 -0500

    adding loading bar for admin invitations page

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,11 @@
 import {Route, Switch} from "react-router-dom";
 import React from "react";
-import Invitation from "./components/Invitation";
 import Invitations from "./components/Invitations";
 import Home from "./components/Home";
 
 function Routes() {
     return (
         <Switch>
-            <Route path="/invitations/:id">
-                <Invitation/>
-            </Route>
             <Route path="/invitations">
                 <Invitations/>
             </Route>
--- src/components/Invitations.js
+++ src/components/Invitations.js
@@ -1,20 +1,32 @@
 import React, {useEffect, useState} from 'react';
-import {Container} from "@mui/material";
+import {Container, LinearProgress} from "@mui/material";
 import InvitationList from "./InvitationList";
 import NewInvitationForm from "./NewInvitationForm";
 import {getAllInvitations} from "../services/invitation.service";
 
 function Invitations() {
     const [invitationList, setInvitationList] = useState([]);
-    const [reload, setReload] = useState(Date())
-    const refresh = () => setReload(Date())
+    const [reload, setReload] = useState(Date());
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(true);
+    const refresh = () => setReload(Date());
     useEffect(() => {
         getAllInvitations().then(data => {
             setInvitationList(data);
         }).catch(error => {
             console.error(error)
-        })
+            setError(error.message)
+        }).finally(() => setLoading(false));
     }, [reload]);
+    if (error !== '') {
+        return (
+            <Container style={{height: "600px"}}>
+                <h1>Error</h1>
+                <p>{error}</p>
+            </Container>
+        )
+    }
+    if (loading) return <LinearProgress/>
     return (
         <Container style={{height: "600px"}}>
             <h1>Invitations</h1>
