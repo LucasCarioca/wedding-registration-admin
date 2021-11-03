89e132c011dfff2a7369c1ea277f089d59e79eeb
Lucas Desouza <lucasfds2777@gmail.com>
2021-11-02 23:46:42 -0500

    feat: add in invitation cards link and style update

--- src/components/InvitationCard.js
+++ src/components/InvitationCard.js
@@ -6,15 +6,19 @@ import QRCode from 'react-qr-code';
 function InvitationCard({name, registrationKey, guestCount}) {
   const url = `${getGuestRegistrationHost()}/registrations/${registrationKey}`;
   return (
-    <Paper style={{padding: '2rem', margin: '1rem', textAlign: 'center'}}>
-      <h3>{name}</h3>
-      <br/>
-      <QRCode value={url} />
-      <br/>
-      <p>{registrationKey}</p>
-      <p>guests: {guestCount}</p>
-      <a href={url}>Register guests</a>
-    </Paper>
+    <>
+      <Paper style={{padding: '2rem', margin: '1rem', textAlign: 'center'}}>
+        <h3>{name}</h3>
+        <br/>
+        <QRCode value={url} />
+        <br/>
+        <p>{registrationKey}</p>
+        <p>guests: {guestCount}</p>
+      </Paper>
+      <div style={{display: 'flex', justifyContent: 'flex-end', paddingRight: '1rem', margin: '0'}}>
+        <a href={url}>registration page &raquo;</a>
+      </div>
+    </>
   );
 }
 
