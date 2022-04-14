cc2a24736282fc74885851abbe8bc4453e4d6ff1
Lucas Desouza <lucasfds2777@gmail.com>
2022-04-14 00:55:01 -0500

    improved navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,80 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import {AppBar, Box, Button, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, Toolbar} from '@mui/material';
 import Login from './Login';
-import styled from '@emotion/styled';
-
-const StyledLink = styled(Link)`
-    text-decoration: none;
-
-    &:focus, &:hover, &:visited, &:link, &:active {
-        text-decoration: none;
-    }
-`;
+import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
+import AppsIcon from '@mui/icons-material/Apps';
 
 function NavBar() {
+  const [drawerState, setDrawerState] = useState(false);
   return (
-    <Box sx={{flexGrow: 1}}>
-      <AppBar position="static" color="transparent">
-        <Toolbar>
-          <div style={{flexGrow: '1'}}>
-            <StyledLink to="/"><Button color="inherit">Home</Button></StyledLink>
-            <StyledLink to="/invitations"><Button color="inherit">Invitations</Button></StyledLink>
-            <StyledLink to="/invitation-cards"><Button color="inherit">cards</Button></StyledLink>
+    <>
+      <Drawer
+        anchor="left"
+        open={drawerState}
+        onClose={() => setDrawerState(!drawerState)}
+      >
+        <List sx={{height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+          <div onClick={() => setDrawerState(!drawerState)}>
+            <Link className="styled-nav-link" to="/">
+              <ListItem>
+                <ListItemIcon>
+                  <HomeIcon/>
+                </ListItemIcon>
+                <ListItemText>
+                  Home
+                </ListItemText>
+              </ListItem>
+            </Link>
+            <Divider/>
+            <Link className="styled-nav-link" to="/invitations">
+              <ListItem>
+                <ListItemIcon>
+                  <AppsIcon/>
+                </ListItemIcon>
+                <ListItemText>
+                  Invitations
+                </ListItemText>
+              </ListItem>
+            </Link>
+            <Link className="styled-nav-link" to="/invitation-cards">
+              <ListItem>
+                <ListItemIcon>
+                  <AppsIcon/>
+                </ListItemIcon>
+                <ListItemText>
+                  Invitation Cards
+                </ListItemText>
+              </ListItem>
+            </Link>
           </div>
-          <Login/>
-        </Toolbar>
-      </AppBar>
-    </Box>
+          <div>
+            <Login/>
+          </div>
+        </List>
+      </Drawer>
+      <Box sx={{flexGrow: 1}}>
+        <AppBar position="static" color="transparent">
+          <Toolbar>
+            <div style={{display: 'flex', justifyContent: 'flex-start', width: '100%'}}>
+              <div>
+                <Button onClick={() => setDrawerState(!drawerState)} id="catalog">
+                  <MenuIcon/>
+                </Button>
+              </div>
+              <div>
+                <Link className="styled-nav-link" to="/">
+                  <Button id="catalog">
+                    Wedding Admin
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
   );
 }
 
